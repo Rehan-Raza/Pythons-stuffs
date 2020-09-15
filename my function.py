2f12ef1805ce9878cd167d151bdc1dfcaf87f950
Rehan-Raza <rehanrazadotcom@gmail.com>
2020-09-15 21:51:25 +0530

    user defined function

---
+++ my function.py
@@ -0,0 +1,10 @@
+def greet(lang):
+    if lang == 'es':
+        print('HOLA !')
+    elif lang == 'fr':
+        print('BONJOUR !')
+    else:
+        print('HELLO!')
+greet('es')
+greet('fr')
+greet('lwda')            
